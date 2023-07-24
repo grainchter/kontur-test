52e2fe3ef70ba1fc7f08a74e36c3e38778e53097
Yana Kazakova <grainchter@gmail.com>
2023-07-24 13:32:47 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+  jQuery(function ($) {
+    $("#phone_input").mask('+7(999) 999 9999');
+  });
+
+});
+
+$(window).on("scroll", function () {
+  $("header").toggleClass("scroll", $(this).scrollTop() > 70);
+  $(".header-burger").removeClass("active", $(this).scrollTop() < 70);
+  $(".line2").removeClass("line2-active", $(this).scrollTop() < 70);
+  $(`div[name="dropdown"`).toggleClass("dropdown", $(this).scrollTop() < 70);
+});
+
+const openBurgerMenu = () => {
+  $(".header-burger").toggleClass("active");
+  $(".line2").toggleClass("line2-active");
+  $(`div[name="dropdown"`).removeClass("dropdown");
+};
